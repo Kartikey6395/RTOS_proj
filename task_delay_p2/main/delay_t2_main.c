cbf6f57c29aa5ebd48e226082794201213d1ea20
Kartikey6395 <kartikeytripathi6395@gmial.com>
2022-01-23 17:33:32 +0530

    delay with diff priority:observed priority preemption scheduling

--- task_delay_p2/main/delay_t2_main.c
+++ task_delay_p2/main/delay_t2_main.c
@@ -4,12 +4,15 @@
 
 
 void Task_1_LED(void *pvParameters)
-{
+{   int i = 0;
     while(1)
-    {
+    {   
         printf("task_1_led_\n");
-        //const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
+        i++;
+        if(i==1000)
+        {//const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
         vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
     }
 }
 
@@ -28,19 +31,19 @@ void app_main()
 {
     TaskHandle_t xHandle_Task_1 = NULL;
     BaseType_t xReturned_1;
-    xReturned_1 = xTaskCreate(Task_1_LED,"TaskDelayLog1",1024,NULL,20,&xHandle_Task_1);
+    xReturned_1 = xTaskCreate(Task_1_LED,"TaskDelayLog1",2048,NULL,20,&xHandle_Task_1);
     TaskHandle_t xHandle_Task_2 = NULL;
     BaseType_t xReturned_2;
-    xReturned_2 = xTaskCreate(Task_2_ALARM,"TaskDelayLog2",1024,NULL,10,&xHandle_Task_2);
+    xReturned_2 = xTaskCreate(Task_2_ALARM,"TaskDelayLog2",2048,NULL,10,&xHandle_Task_2);
     if(xReturned_1 == pdPASS)
     {
-        printf("task 1 created\n");
+        printf("task 1 created: Priority=> %d\n",uxTaskPriorityGet(xHandle_Task_1));
         //vTaskDelete( xHandle_Task_1);
     }
 
     if(xReturned_2 == pdPASS)
     {
-        printf("task 2 created\n");
+        printf("task 2 created: Priority=> %d\n",uxTaskPriorityGet(xHandle_Task_2));
         //vTaskDelete( xHandle_Task_2);
     }
 }
