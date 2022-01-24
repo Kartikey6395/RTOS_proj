9eed21d8d8914cd4d42f8b94b766b90647fffddb
Kartikey6395 <kartikeytripathi6395@gmial.com>
2022-01-25 01:29:42 +0530

    Counting semaphore

--- count_sema/main/count_sema_main.c
+++ count_sema/main/count_sema_main.c
@@ -9,7 +9,7 @@ QueueHandle_t xMsgQueue_1;
 SemaphoreHandle_t sem_count;
 
 
-void Task_1_Sensor(void *pvParameters)
+void Task_1_Sensor_A(void *pvParameters)
 {   int samples = 0,count=0;
     
     while(1)
@@ -17,6 +17,7 @@ void Task_1_Sensor(void *pvParameters)
         
         samples++;
         count++;
+        xSemaphoreTake(sem_count);
         xQueueSend(xMsgQueue_1,&samples,portMAX_DELAY);
         printf("Sensor_task:Sending Data = %d\n",samples);
         vTaskDelay(500 / portTICK_PERIOD_MS);
@@ -29,6 +30,30 @@ void Task_1_Sensor(void *pvParameters)
     }
 }
 
+int arr[10];
+
+void Task_3_Sensor_B(void *pvParameters)
+{   
+    int count =0;
+    while(1)
+    {   
+        count++;
+        xSemaphoreTake(sem_count);
+        xQueueReceive(xMsgQueue_1,&arr,portMAX_DELAY);
+        printf("Sensor B_task_3:Sending Data arr[] = %d\n",arr[count]);
+        &arr++;
+        
+        //vTaskDelay(500 / portTICK_PERIOD_MS);
+        if(count == 10)
+        {   xSemaphoreGive(sem_count);
+            count =0;
+        //const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        }
+    }
+}
+
+
 void Task_2_Process(void *pvParameters)
 {
     
@@ -38,6 +63,7 @@ void Task_2_Process(void *pvParameters)
         xSemaphoreTake( sem_count, portMAX_DELAY);
         xQueueReceive(xMsgQueue_1,&j,portMAX_DELAY);
         printf("Processing_task:receive data =%d\n",j);
+        xSemaphoreGive(sem_count);
         //const TickType_t xDelay = 1000 / portTICK_PERIOD_MS;
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
@@ -48,11 +74,14 @@ void app_main()
 {
     
     xMsgQueue_1 = xQueueCreate( 10,sizeof(int));
-    sem_count = xSemaphoreCreateCounting(1,0);
+    sem_count = xSemaphoreCreateCounting(2,2);
     
     TaskHandle_t xHandle_Task_1 = NULL;
     BaseType_t xReturned_1;
-    xReturned_1 = xTaskCreate(Task_1_Sensor,"Task_1_Sensing",2048,NULL,5,&xHandle_Task_1);
+    xReturned_1 = xTaskCreate(Task_1_Sensor_A,"Task_1_Sensing",2048,NULL,5,&xHandle_Task_1);
+     TaskHandle_t xHandle_Task_3 = NULL;
+    BaseType_t xReturned_3;
+    xReturned_3 = xTaskCreate(Task_3_Sensor_B,"Task_3_Sensing",2048,NULL,5,&xHandle_Task_3);
     TaskHandle_t xHandle_Task_2 = NULL;
     BaseType_t xReturned_2;
     xReturned_2 = xTaskCreate(Task_2_Process,"Task_2_Processing",2048,NULL,5,&xHandle_Task_2);
