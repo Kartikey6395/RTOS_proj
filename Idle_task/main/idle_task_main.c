eb7936fb2a8638b607dfb8911f82f210ee006773
Kartikey6395 <kartikeytripathi6395@gmial.com>
2022-01-27 01:13:19 +0530

    Idle Hook callback function

--- Idle_task/main/idle_task_main.c
+++ Idle_task/main/idle_task_main.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <esp_freertos_hooks.h>
 
 
 
 int i = 0;
-TickType_t TickCount;
+TickType_t TickCount_1,TickCount_2,TickCount_3;
 void Task_1(void *pvParameters)
 {
     while(1)
     {
         printf("INC = %d\n",i++);
         vTaskDelay(2000 / portTICK_PERIOD_MS);
-        TickCount = xTaskGetTickCount();
-        printf("countTICKS_Task_1 = %d\n",TickCount);
+        TickCount_1 = xTaskGetTickCount();
+        printf("countTICKS_Task_1 = %d\n",TickCount_1);
     }
 }
 
@@ -21,18 +22,24 @@ void IdleTaskHook(void *pvParameters)
 {
     while(1)
     {
-        TickCount = xTaskGetTickCount();
-        printf("countTICKS_Task_Idle = %d\n",TickCount);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        TickCount_2 = xTaskGetTickCount();
+        printf("countTICKS_Task_Idle = %d\n",TickCount_2);
+        
+        //vTaskDelay(2000 / portTICK_PERIOD_MS);
         
     }
 }
- //void vApplicationIdleHook(void);
- /*{
-     printf("This is Idle_Task_Hooking\n");
-     TickCount = xTaskGetTickCount();
-     printf("countTICKS_Idle_task = %d\n",TickCount);
- }*/
+ bool vApplicationIdleHook()
+ {
+     printf("This is Idle_Task_Hooking_cb function\n");
+     TickCount_2 = xTaskGetTickCount();
+     printf("countTICKS_Idle_task = %d\n",TickCount_2);
+    return true;
+ }
+ void vApplicationTickHook()
+ {
+     return;
+ }
 
 
 void app_main()
@@ -40,13 +47,15 @@ void app_main()
     TaskHandle_t xHandle_Task_1 =NULL;
    TaskHandle_t xHandle_Task_Idle =NULL;
     BaseType_t xReturned_1;
-    BaseType_t xReturned_2;
-   TickCount = xTaskGetTickCount();
-    printf("countTICKS_main = %d\n",TickCount);
+   BaseType_t xReturned_2;
+   TickCount_3 = xTaskGetTickCount();
+    printf("countTICKS_main = %d\n",TickCount_3);
    xReturned_1 = xTaskCreate(Task_1,"TaskLog_1",2048,NULL,5,&xHandle_Task_1);
    xReturned_2 = xTaskCreate(IdleTaskHook,"TaskLog_Idle",2048,NULL,tskIDLE_PRIORITY,&xHandle_Task_Idle);
-    xTaskGetIdleTaskHandle();
-
+    //xTaskGetIdleTaskHandle();
+    esp_register_freertos_idle_hook(vApplicationIdleHook);
+    esp_register_freertos_tick_hook(vApplicationTickHook);
+    vTaskDelay(1000/portTICK_PERIOD_MS);
    if(xReturned_1 == pdPASS)
    {
        printf("Task_1 created \n");
